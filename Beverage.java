385003f6d6bdeef806d70f35acc01e62afecbefa
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-09-03 17:52:49 -0300

    Create Beverage.java

---
+++ Beverage.java
@@ -0,0 +1,14 @@
+public abstract class Beverage {
+	private String description;
+	
+	public Beverage(String description){
+		super();
+		this.description = description;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public abstract double cost();
+}
