1a342e8654b195e92dab336d04287af776ec069c
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-09-03 17:53:45 -0300

    Create AddOn.java

---
+++ AddOn.java
@@ -0,0 +1,12 @@
+public abstract class AddOn extends Beverage{
+	
+	protected Beverage beverage;
+	
+	public AddOn(String description, Beverage bev){
+		super(description);
+		this.beverage = bev;
+	}
+	
+	public abstract String getDescription();
+
+}
